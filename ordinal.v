226e77bed957d0f103d46cf3eb647ad44ae7f291
Rob Dockins <rdockins@galois.com>
2021-11-03 15:43:01 -0700

    some futzing about

--- ordinal.v
+++ ordinal.v
@@ -212,14 +212,19 @@ Proof.
   apply ord_le_refl.
 Defined.
 
+Definition ordinal_induction
+  : forall P : Ord -> Set,
+     (forall x : Ord, (forall y : Ord, y < x -> P y) -> P x) ->
+     (forall a : Ord, P a)
+  := well_founded_induction ord_lt_wf.
+
 (** The less-than order is irreflexive, a simple corollary of well-foundedness.
   *)
 Corollary ord_lt_irreflexive : forall x, x < x -> False.
 Proof.
-  induction x using (well_founded_induction ord_lt_wf).
+  induction x using ordinal_induction.
   firstorder.
 Qed.
-
 
 (** * Ordinal equality is an equality relation *)
 
@@ -309,7 +314,10 @@ Fixpoint glbOrd (x y:Ord) : Ord :=
   end.
 Notation "x ⊓ y" := (glbOrd x y) (at level 55, right associativity) : ord_scope.
 
-
+Definition boundedSup (β:Ord) (f:Ord -> Ord) : Ord :=
+  match β with
+  | ord B g => supOrd (fun i => f (g i))
+  end.
 
 Definition predOrd (x:Ord) : Ord :=
   match x with
@@ -547,6 +555,29 @@ Proof.
     red; intros; apply H.
 Qed.
 
+
+Lemma boundedSup_le β (f:Ord -> Ord) :
+  (forall x y, x ≤ y -> f x ≤ f y) ->
+  forall x, x < β -> f x ≤ boundedSup β f.
+Proof.
+  intro Hmono.
+  destruct β as [B g].
+  simpl; intros.
+  rewrite ord_lt_unfold in H.
+  destruct H as [b Hb].
+  rewrite <- (sup_le _ _ b).
+  apply Hmono. apply Hb.
+Qed.
+
+Lemma boundedSub_least β (f:Ord -> Ord) z :
+  (forall x, x < β -> f x ≤ z) ->
+  boundedSup β f ≤ z.
+Proof.
+  destruct β as [B g]. simpl. intros.
+  apply sup_least.
+  intros. apply H.
+  apply (index_lt (ord B g)).
+Qed.
 
 (** Functions into sized types have sizes defined by nontrivial
     limit ordinals.
@@ -2459,6 +2490,164 @@ Proof.
 Qed.
 Hint Resolve In_lt : ord.
 
+Lemma listOrd_bounded_aux A (f:A -> Ord) l :
+  listOrd f l ≤ (ord A f) ⊗ (length l : ω).
+Proof.
+  induction l; simpl.
+  apply zero_least.
+  apply succ_least.
+  rewrite (addOrd_comm (f a)).
+  apply ord_lt_le_trans with (listOrd f l ⊕ (ord A f)).
+  apply addOrd_monotone_lt2. apply (index_lt (ord A f)).
+  rewrite <- (sup_le _ _ tt).
+  apply addOrd_monotone_le; auto with ord.
+Qed.
+
+Lemma listOrd_bounded (A:Ord) (l:list A) :
+  sz l ≤ A ⊗ ω.
+Proof.
+  destruct A as [A f].
+  simpl. rewrite <- (sup_le _ _ (length l)).
+  rewrite listOrd_bounded_aux; auto with ord.
+Qed.
+
+Lemma listOrd_bounded' (A:Ord) (l:list A) :
+  sz l < (succOrd A) ⊗ ω.
+Proof.
+  destruct A as [A f].
+  simpl. rewrite <- (sup_le _ _ (length l)).
+  rewrite addOrd_succ2.
+  apply succ_trans.
+  rewrite <- addOrd_le1.
+  rewrite listOrd_bounded_aux; auto with ord.
+  simpl.
+  apply mulOrd_monotone_le1.
+  auto with ord.
+Qed.
+
+(* Streams, represented as functions from natural numbers to values *)
+Definition stream (A:Type) := nat -> A.
+
+Definition streamIdx {A:Type} (n:nat) (s:stream A) : A := s n.
+
+Definition streamCons {A:Type} (a:A) (s:stream A) : stream A :=
+  fun n => match n with
+           | O => a
+           | S n' => streamIdx n' s
+           end.
+
+Fixpoint streamAppend {A:Type} (l:list A) (s:stream A) : stream A :=
+  match l with
+  | [] => s
+  | (h::tl) => streamCons h (streamAppend tl s)
+  end.
+
+(* The ordinal size of a stream.  We carefully arrange it so that
+   list prefixes of streams are subterms; this is the reason for
+   the (succ A ⊗ ω) term.  Moreover, the leading succ ensures that
+   the elements of streams are always subterms of the stream.
+ *)
+Definition streamOrd {A} (f:A -> Ord) (s:stream A) : Ord :=
+  succOrd (supOrd (fun n => f (streamIdx n s) ⊕ (succOrd (ord A f) ⊗ ω))).
+Canonical Structure StreamOrd (A:Ord) : Ord :=
+  ord (stream A) (streamOrd (ordSize A)).
+
+Lemma streamIdx_lt (A:Ord) (s:stream A) n :
+  streamIdx n s ◃ s.
+Proof.
+  simpl. unfold streamOrd.
+  eapply ord_le_lt_trans; [ | apply succ_lt ].
+  rewrite <- (sup_le _ _ n). simpl.
+  auto with ord.
+Qed.
+
+Lemma streamHead_lt (A:Ord) (h:A) (tl:stream A) :
+  h ◃ streamCons h tl.
+Proof.
+  simpl. unfold streamOrd.
+  eapply ord_le_lt_trans; [ | apply succ_lt ].
+  rewrite <- (sup_le _ _ 0). simpl.
+  auto with ord.
+Qed.
+
+Lemma streamTail_le (A:Ord) (h:A) (tl:stream A) :
+  tl ⊴ streamCons h tl.
+Proof.
+  simpl. unfold streamOrd.
+  apply succ_monotone_le.
+  apply sup_least. intro i.
+  rewrite <- (sup_le _ _ (S i)).
+  simpl.
+  auto with ord.
+Qed.
+
+Lemma streamAppend_lt1 (A:Ord) (xs:list A) (ys:stream A) :
+  xs ◃ streamAppend xs ys.
+Proof.
+  induction xs; intros; simpl; auto with ord.
+  - unfold streamOrd.
+    apply succ_trans; apply zero_least.
+  - unfold streamOrd.
+    apply succ_monotone_lt.
+    rewrite <- (sup_le _ _ 0).
+    unfold streamIdx; unfold streamCons.
+    apply addOrd_monotone_lt2; auto with ord.
+    destruct A; apply listOrd_bounded'.
+Qed.
+
+Lemma streamAppend_le2 (A:Ord) (xs:list A) (ys:stream A) :
+  ys ⊴ streamAppend xs ys.
+Proof.
+  revert ys; induction xs; intros; simpl; auto with ord.
+  etransitivity. apply IHxs.
+  apply streamTail_le.
+Qed.
+
+(* Countably-wide rose trees. *)
+Inductive CountableRoseTree (A:Type) :=
+  | RoseNode : A -> (nat -> CountableRoseTree A) -> CountableRoseTree A.
+
+Fixpoint countableRoseOrd (A:Type) (f:A -> Ord) (t:CountableRoseTree A) : Ord :=
+  match t with
+  | RoseNode _ a s => succOrd (f a ⊕ succOrd (supOrd (fun n => countableRoseOrd A f (s n))))
+  end.
+Canonical Structure CountableRoseOrd (A:Ord) : Ord :=
+  ord (CountableRoseTree A) (countableRoseOrd _ A).
+
+Lemma rose_elem_lt (A:Ord) (a:A) (s:nat -> CountableRoseTree A) :
+  a ◃ RoseNode _ a s.
+Proof.
+  simpl; auto with ord.
+Qed.
+
+Lemma rose_immediate_subtree_lt (A:Ord) (a:A) (s:nat -> CountableRoseTree A) n :
+  s n ◃ RoseNode _ a s.
+Proof.
+  simpl.
+  apply succ_trans.
+  rewrite <- addOrd_le2.
+  apply ord_lt_le; apply succ_trans.
+  rewrite <- (sup_le _ _ n).
+  auto with ord.
+Qed.
+
+Inductive is_subtree {A} (sub:CountableRoseTree A) : CountableRoseTree A  -> Prop :=
+| immediate_subtree : forall a s n, streamIdx n s = sub -> is_subtree sub (RoseNode _ a s)
+| distant_subtree : forall a s n, is_subtree sub (s n) -> is_subtree sub (RoseNode _ a s).
+
+Lemma rose_subtree_lt (A:Ord) (sub t:CountableRoseTree A) : is_subtree sub t -> sub ◃ t.
+Proof.
+  intro H; induction H.
+  - rewrite <- H. apply rose_immediate_subtree_lt.
+  - rewrite IHis_subtree. apply rose_immediate_subtree_lt.
+Qed.
+
+Lemma rose_acyclic (A:Ord) (sub t:CountableRoseTree A) (H:is_subtree sub t) : sub <> t.
+Proof.
+  apply size_discriminate.
+  apply rose_subtree_lt; auto.
+Qed.
+
 
 (** Let's play around with some Ltac support.
 
@@ -2523,36 +2712,6 @@ Canonical Structure oddOrdSize :=
 Lemma even_odd_neq : forall x, x <> oddS (evenS (oddS (evenS x))).
 Proof.
   ord_crush.
-Qed.
-
-
-(** Countably-wide rose trees *)
-
-Inductive tree :=
-| leaf : tree
-| node : (nat -> tree) -> tree.
-
-Fixpoint tree_size (t:tree) : Ord :=
-  match t with
-  | leaf => zeroOrd
-  | node f => funOrd tree_size f
-  end.
-
-Canonical Structure treeOrdSize :=
-  ord tree tree_size.
-
-(* Not entirely sure how to automate this proof better... *)
-Goal forall x f g n m ,
-    g m = x ->
-    f n = node g ->
-    x <> node f.
-Proof.
-  intros. apply size_discriminate.
-  apply subterm_trans with (f n).
-  rewrite H0.
-  rewrite <- H.
-  apply fun_lt.
-  apply fun_lt.
 Qed.
 
 (** A more complicated example using mutual recursion,
